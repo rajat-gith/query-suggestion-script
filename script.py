b23570f297c2014d263ea7f8b24b3b983fe2b03d
rajat-gith <rajatnayak1582002@gmail.com>
2024-12-26 12:59:58 +0530

    added changes to deploy

--- script.py
+++ script.py
@@ -109,5 +109,5 @@ async def process_query(request:TableRequest):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
