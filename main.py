1455db8ceaef5f029a9025ec124f97b32e5c8c6a
rajat-gith <rajatnayak1582002@gmail.com>
2024-12-26 13:08:04 +0530

    made deployment changes

--- main.py
+++ main.py
@@ -106,8 +106,7 @@ async def process_query(request:TableRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
